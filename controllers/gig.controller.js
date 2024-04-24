7aacffa17dc53bdafe8ccaae64b56b3e0752ae3c
“Ebrahim1911” <ebrahimn709@hmail.com>
2024-04-24 15:45:11 +0200

    Modify GIG-Controller

--- controllers/gig.controller.js
+++ controllers/gig.controller.js
@@ -53,7 +53,7 @@ const getGigs = async (req, res, next) => {
   };
   try {
     const gigs = await Gig.find(filters).sort({ [q.sort]: -1 });
-    res.status(200).send(gigs);
+    res.status(200).json(gigs);
   } catch (err) {
     next(err);
   }

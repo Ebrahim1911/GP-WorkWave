f36a74a09c64b2efd3ea84d208b96fa53b3bbd59
“Ebrahim1911” <ebrahimn709@hmail.com>
2024-05-04 00:33:35 +0300

    Test Reviews Required🚩

--- models/review.model.js
+++ models/review.model.js
@@ -5,28 +5,34 @@ const ReviewSchema = new Schema(
   {
     gigId: {
       type: String,
-      required: true,
+      // required: true,
+      required: false,
     },
     userId: {
       type: String,
-      required: true,
+      // required: true,
+      required: false,
     },
     star: {
       type: Number,
-      required: true,
+      // required: true,
+      required: false,
       enum: [1, 2, 3, 4, 5],
     },
     desc: {
       type: String,
-      required: true,
+      // required: true,
+      required: false,
     },
     img: {
       type: String,
-      required: true,
+      // required: true,
+      required: false,
     },
     username: {
       type: String,
-      required: true,
+      // required: true,
+      required: false,
     },
   },
   {

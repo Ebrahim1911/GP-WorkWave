c331dbf063e15a749408e93bc57caec5f52f3f5d
“Ebrahim1911” <ebrahimn709@hmail.com>
2024-05-12 05:42:56 +0300

    Add Admin role to users

--- models/user.model.js
+++ models/user.model.js
@@ -40,6 +40,10 @@ const UserSchema = new Schema(
       type: Boolean,
       default: false,
     },
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
     favorties: [],
   },
   {
